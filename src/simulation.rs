78320ecd7cb1c82c8209ed4b61f9a781cb705f6e
Federico Pasqua <federico.pasqua.96@gmail.com>
2018-03-11 17:10:52 +0100

    Implement simulation.rs

--- src/simulation.rs
+++ src/simulation.rs
@@ -5,16 +5,19 @@ use num::{Float,Integer};
 use vector::Vector2;
 use sim_elements::{Simulable,Planet};
 
+const default_G: f64 = 5.6;
+
 struct Simulation<T,U> where T: Float, U: Integer{
     delta_t: U,
-    total_t: U,
+    total_rel_t: U,
+    G: T,
     sim_bodies: Vec<Box<Simulable<T,U>>>,
 }
 
 impl<T,U> Simulation<T,U> where T: Float, U: Integer {
     fn new(time_granularity: U) -> Simulation<T,U> {
         assert!(time_granularity != num::zero());
-        Simulation{ delta_t: time_granularity, total_t: num::zero(), sim_bodies: Vec::new() }
+        Simulation{ delta_t: time_granularity, total_rel_t: num::zero(), sim_bodies: Vec::new(), G: default_G as T }
     }
 
     fn add_body(&mut self body: Box<Simulable<T,U>>) {
@@ -22,16 +25,21 @@ impl<T,U> Simulation<T,U> where T: Float, U: Integer {
     }
 
     fn evolve_single_delta_t(&mut self) {
-        unimplemented!()
+        for body_box in self.sim_bodies.iter_mut() {
+            body_box.simulate_step(&sim_bodies, self.delta_t, self.G);
+        }
+        self.total_rel_t += 1
     }
 
     fn evolve(&mut self, num_of_step: U) {
-        unimplemented!()
+        for i in 0..num_of_step {
+            self.evolve_single_delta_t();
+        }
     }
 }
 
 impl<T,U> fmt::Display for Simulation<T,U> where T: Float, U: Integer {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.delta_t, self.total_t)
+        write!(f, "({}, {})", self.delta_t, self.total_rel_t)
     }
 }

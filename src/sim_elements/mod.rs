da3189dbffa49f0ecae16c5a26f2724c6fc6505c
Federico Pasqua <federico.pasqua.96@gmail.com>
2018-03-11 13:01:47 +0100

    Use num::Float for more restricted generics

--- src/sim_elements/mod.rs
+++ src/sim_elements/mod.rs
@@ -1,33 +1,37 @@
 extern crate num;
 
 use vector::Vector2;
+use num::Float;
+use num::Integer;
 
-#[derive(Debug)]
-struct CorpoCeleste {
-    position: Vector2<f64>,
-    velocity: Vector2<f64>,
-    mass: f64,
-    name: String,
+pub trait Simulable<T,U> where T: Float, U: Integer {
+    fn get_name(&self) -> String;
+    fn get_position(&self) -> Vector2<T>;
+    fn get_velocity(&self) -> Vector2<T>;
+    fn get_mass(&self) -> T;
+    fn simulate_step(&mut self, other_sim: Vec<Box<Simulable<T,U>>>, delta_t: U);
 }
 
-impl CorpoCeleste {
-    fn new(name: &str, mass: f64, pos: Vector2<f64>, vel: Vector2<f64>) -> CorpoCeleste {
-        CorpoCeleste { position: pos, velocity: vel, mass: mass, name: String::from(name) }
-    }
+pub struct Planet<T> where T: Float {
+    name: String,
+    pos: Vector2<T>,
+    vel: Vector2<T>,
+    mass: T,
+}
 
-    fn pos(self) -> Vector2<f64> {
-        self.position
+impl<T> Planet<T> where T: Float {
+    fn new(name: &str, pos: Vector2<T>, vel: Vector2<T>, mass: T) -> Planet<T> {
+        //TODO: Check mass > 0 (?)
+        Planet { name: String::from(name), pos, vel, mass }
     }
+}
 
-    fn vel(self) -> Vector2<f64> {
-        self.velocity
-    }
-
-    fn mass(self) -> f64 {
-        self.mass
-    }
-
-    fn name(self) -> String {
-        self.name
+impl<T,U> Simulable<T,U> for Planet<T> where T: Float, U: Integer {
+    fn get_name(&self) -> String { self.name.clone() }
+    fn get_position(&self) -> Vector2<T> { self.pos }
+    fn get_velocity(&self) -> Vector2<T> { self.vel }
+    fn get_mass(&self) -> T { self.mass }
+    fn simulate_step(&mut self, other_sim: Vec<Box<Simulable<T,U>>>, delta_t: U) {
+        unimplemented!()
     }
 }
